c13b78a23ae54fab3f4e50cb8b41b44f3eda3cdb
Keenan <keenanjj13@gmail.com>
2023-05-24 12:36:37 -0500

    refactor(highlight): changed autocmd TextChangeI to run on just the lin

--- lua/bionic-reading/init.lua
+++ lua/bionic-reading/init.lua
@@ -1,14 +1,10 @@
 local config = require("bionic-reading.config")
-local highlight = require("bionic-reading.highlight")
+require("bionic-reading.highlight")
 
 local M = {}
 
 function M.setup(opts)
   config.extend(opts)
-
-  highlight.insert_highlights()
-  highlight.create_autocmds()
-  highlight.create_user_commands()
 end
 
 return M
